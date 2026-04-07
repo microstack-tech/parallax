16cfd3712e19b6b98982cd5d4bbb8edb4d548fc8
andrepatta <9854773+andrepatta@users.noreply.github.com>
2026-04-07 09:07:58 -0300

    prlconfig: make estimateSmartFee enabled by default

--- prl/prlconfig/config.go
+++ prl/prlconfig/config.go
@@ -41,12 +41,13 @@ import (
 
 // FullNodeGPO contains default gasprice oracle settings for full node.
 var FullNodeGPO = gasprice.Config{
-	Blocks:           20,
-	Percentile:       60,
-	MaxHeaderHistory: 1024,
-	MaxBlockHistory:  1024,
-	MaxPrice:         gasprice.DefaultMaxPrice,
-	IgnorePrice:      gasprice.DefaultIgnorePrice,
+	Blocks:                  20,
+	Percentile:              60,
+	MaxHeaderHistory:        1024,
+	MaxBlockHistory:         1024,
+	MaxPrice:                gasprice.DefaultMaxPrice,
+	IgnorePrice:             gasprice.DefaultIgnorePrice,
+	EnableSmartFeeEstimator: true,
 }
 
 // LightClientGPO contains default gasprice oracle settings for light client.
